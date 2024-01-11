71bcbfc3c41b4194746de4f51638ea3f9faf00c4
azka-begh <97597065+azka-begh@users.noreply.github.com>
2024-01-11 20:02:46 +0530

    Update cluster.sh

--- k8s/cluster.sh
+++ k8s/cluster.sh
@@ -9,27 +9,33 @@ KEY_NAME=docker-nexus
 aws sts get-caller-identity >> /dev/null
 if [ $? -eq 0 ]
 then
-  echo "Credentials tested, proceeding with the cluster creation."
-
-  # Creation of EKS cluster
-  eksctl create cluster \
-  --name $CLUSTER_NAME \
-  --version 1.27 \
-  --region $REGION \
-  --nodegroup-name $NODE_NAME \
-  --nodes 2 \
-  --nodes-min 1 \
-  --nodes-max 2 \
-  --node-type t3.micro \
-  --node-volume-size 8 \
-  --ssh-access \
-  --ssh-public-key $KEY_NAME \
-  --managed
-  if [ $? -eq 0 ]
+ echo "Credentials tested, proceeding with the cluster creation."
+ OUTPUT=$(eksctl get cluster --region $REGION)
+  if [ "$OUTPUT" == "No clusters found" ]
   then
-    echo "Cluster Setup Completed with eksctl command."
+    echo "$OUTPUT... Creating cluster(s)"
+    # Creation of EKS cluster
+    eksctl create cluster \
+    --name $CLUSTER_NAME \
+    --version 1.27 \
+    --region $REGION \
+    --nodegroup-name $NODE_NAME \
+    --nodes 2 \
+    --nodes-min 1 \
+    --nodes-max 2 \
+    --node-type t3.micro \
+    --node-volume-size 8 \
+    --ssh-access \
+    --ssh-public-key $KEY_NAME \
+    --managed
+    if [ $? -eq 0 ]
+    then
+      echo "Cluster Setup Completed with eksctl command."
+    else
+      echo "Cluster Setup Failed while running eksctl command."
+    fi
   else
-    echo "Cluster Setup Failed while running eksctl command."
+    echo "Cluster already exists. Proceeding with deployment"
   fi
 else
   echo "Please run aws configure & set right credentials."
